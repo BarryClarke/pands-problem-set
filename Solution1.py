3d5ce8f5b60a079a457339938aaa2d307c9e4ed8
Barry Clarke <G00376293@gmit.ie>
2019-02-23 04:15:54 +0000

    revised and improved Solution1

--- Solution1.py
+++ Solution1.py
@@ -1,15 +1,21 @@
 #Program to sum all numbers between 1 and a positive integer inputted by the user
-#Barry Clarke 18th Jan 2019
-#Rev0 - Basic operation 
+#Barry Clarke 22nd Jan 2019
+#Rev1 - Operation to include erroneous entries
 
-i = int(input("Please enter a positive integer: "))
-
-if i < 0:
-      i = int(input("This is a negative integer. Please enter a positive integer: "))
-
-Sum = 0
-
-while i >= 0:
-   Sum = Sum + i
-   i = i - 1
-print(Sum)
+#Reference: https://stackoverflow.com/questions/26198131/check-if-input-is-positive-integer
+while True:
+    try:
+        i = int(input("Please enter a positive integer: "))
+        n = i
+        if i < 0:                                           # In the event of a negative integer being inputted by the user
+            print("\nError: This is a negative integer")
+            continue 
+        else:
+            Total = 0
+            while i >= 0:
+                Total = Total + i
+                i = i - 1
+            print("\nThe Sum of all positive integers up to", n , "is", Total)
+        break                                               # breaks from the loop as this is the line where the program ends and reaches a result
+    except ValueError:                                      # Used in the case of any non-integer inputted by the user
+        print("\nError: This entry is not a positive integer") 
